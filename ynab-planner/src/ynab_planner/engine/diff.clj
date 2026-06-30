abe2816ba8b3c12df50ea2b37285726e52620471
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2026-06-29 22:38:17 -0500

    feat(engine): diff carries group-name; grouped-diff by YNAB group

--- ynab-planner/src/ynab_planner/engine/diff.clj
+++ ynab-planner/src/ynab_planner/engine/diff.clj
@@ -5,5 +5,11 @@
   [categories]
   (->> categories
        (filter #(not= (:ynab-current %) (:planned %)))
-       (mapv (fn [c] {:id (:id c) :name (:name c)
+       (mapv (fn [c] {:id (:id c) :name (:name c) :group-name (:group-name c)
                       :current (:ynab-current c) :planned (:planned c)}))))
+
+(defn grouped-diff
+  "Group changed-category diff items by :group-name, preserving first-appearance order."
+  [diff]
+  (vec (for [g (distinct (map :group-name diff))]
+         {:group-name g :items (filterv #(= g (:group-name %)) diff)})))
--- ynab-planner/test/ynab_planner/engine/diff_test.clj
+++ ynab-planner/test/ynab_planner/engine/diff_test.clj
@@ -9,5 +9,18 @@
         result (diff/plan-diff cats)]
     (is (= 2 (count result)))
     (is (= #{"a" "c"} (set (map :id result))))
-    (is (= {:id "a" :name "Arriendo" :current 1500000 :planned 1600000}
+    (is (= {:id "a" :name "Arriendo" :group-name nil :current 1500000 :planned 1600000}
            (first (filter #(= "a" (:id %)) result))))))
+
+(deftest plan-diff-carries-group-name
+  (let [cats [{:id "c1" :name "A" :group-name "🏡 Hogar" :ynab-current 100 :planned 200}]
+        out  (diff/plan-diff cats)]
+    (is (= "🏡 Hogar" (:group-name (first out))))))
+
+(deftest grouped-diff-preserves-first-appearance-order
+  (let [diff [{:id "a" :name "A" :group-name "🏡 Hogar"  :current 1 :planned 2}
+              {:id "b" :name "B" :group-name "🎉 Diversión" :current 1 :planned 2}
+              {:id "c" :name "C" :group-name "🏡 Hogar"  :current 1 :planned 2}]
+        out  (diff/grouped-diff diff)]
+    (is (= ["🏡 Hogar" "🎉 Diversión"] (mapv :group-name out)))
+    (is (= ["a" "c"] (mapv :id (:items (first out)))))))
